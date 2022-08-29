8c6248fc2063bec158d32c78d6a7a5020392a066
natsumi3hara <108358971+natsumi3hara@users.noreply.github.com>
2022-08-30 02:11:10 +0800

    Added Japanese version of Drops

--- scripts/questname_data.js
+++ scripts/questname_data.js
@@ -26,5 +26,27 @@ const subStoryName = {
     "016":" (Wonders of Geshpent Tour!)",
 };
 
+const subStoryNameJP = {
+    "000":" (ようこそゲシュペンス島へ)",
+    "001":" (狙われたホルスタイン)",
+    "002":" (貧乳同盟の野望)",
+    "003":" (異世界えいりあん)",
+    "004":" (魔女エルフと魔法のランプ)",
+    "005":" (ラストオブパイパイ)",
+    "006":" (ラストオブパイパイ)",
+    "007":" (アチチなHOTトレーニング)",
+    "008":" (アチチなHOTトレーニング)",
+    "009":" (おこぼれにネズミは乗る)",
+    "010":" (おこぼれにネズミは乗る)",
+    "011":" (捨てられクマにキャワワを)",
+    "012":" (捨てられクマにキャワワを)",
+    "013":" (ゴールドバカンスデカダンス)",
+    "014":" (ゴールドバカンスデカダンス)",
+    "015":" (ゲシュペンス島ふしぎめぐり！)",
+    "016":" (ゲシュペンス島ふしぎめぐり！)",
+};
+
 const tableAvailable = '<table id="drop-available"><tr><th style="width:60%">Stage Name (Available)</th><th style="width:10%">Drop Count</th><th style="width:10%">Drop Probability (%)</th><th style="width:20%">Expected stamina to get 1 item<br>(less is better)</th></tr></table>';
 const tableUnavailable = '<table id="drop-available"><tr><th style="width:60%">Stage Name (Unavailable)</th><th style="width:10%">Drop Count</th><th style="width:10%">Drop Probability (%)</th><th style="width:20%">Expected stamina to get 1 item<br>(less is better)</th></tr></table>';
+const tableAvailableJP = '<table id="drop-available"><tr><th style="width:60%">ステージ名（開放中）</th><th style="width:10%">ドロップ数</th><th style="width:10%">ドロップ率（％）</th><th style="width:20%">スタミナ期待値<br>（少ない方が良い）</th></tr></table>';
+const tableUnavailableJP = '<table id="drop-available"><tr><th style="width:60%">ステージ名（未開放）</th><th style="width:10%">ドロップ数</th><th style="width:10%">ドロップ率（％）</th><th style="width:20%">スタミナ期待値<br>（少ない方が良い）</th></tr></table>';
